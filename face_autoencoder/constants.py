83e6ac0b7dac103e7f6df94fd5b72e338c56ee69
Wiktor Łazarski <wjlazarski@gmail.com>
2022-06-07 12:35:03 +0200

    Update default model gdrive id.

--- face_autoencoder/constants.py
+++ face_autoencoder/constants.py
@@ -2,5 +2,5 @@ import os
 
 # fmt: off
 FACE_VAE_MODEL_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), "model", "vae.ckpt")
-FACE_VAE_MODEL_DOWNLOAD_URL = "https://drive.google.com/uc?id=1DWo71f0uFCUz_LXCRKZFeV0Bbgy6fw5o"
+FACE_VAE_MODEL_DOWNLOAD_URL = "https://drive.google.com/uc?id=1UQ2CRMDZbeMlTRJeHhYrq4UkaCSXgXe6"
 # fmt: on
